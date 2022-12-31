1baa4d5dd7628543d0e5cf980ba865e138f8cdf2
Jagad R R <43224845+zenthicmc@users.noreply.github.com>
2022-12-31 22:13:01 +0700

    update iak callback

---
+++ src/callback/IakCallback.js
@@ -0,0 +1,43 @@
+"use strict"
+
+require('../config/database')
+const User = require('../models/User')
+const Transaction = require('../models/Transaction')
+const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const hmacSHA256 = require('crypto-js/hmac-sha256'); 
+const hex = require('crypto-js/enc-hex');
+const { response400, response403, response404, response500 } = require('../helpers/response')
+
+async function handle(req, res) {
+	try {
+		const reference = req.body.data.ref_id
+		const transaction = await Transaction.findOne({ reference: reference })
+
+		if (!transaction) {
+			return response404(res, 'Transaction not found')
+		}
+
+		const status = req.body.data.status
+		if(status == 1) {
+			transaction.status = 'Success'
+			transaction.save()
+
+			const user = await User.findOne({ _id: transaction.user_id })
+			user.saldo = user.saldo - transaction.amount
+			user.save()
+		} else {
+			transaction.status = 'Failed'
+			transaction.save()
+		}
+	}
+	catch (err) {
+		console.log(err)
+		return response500(res)
+	}
+}
+
+module.exports = {
+	handle
+}
--- src/routes/CallbackRoutes.js
+++ src/routes/CallbackRoutes.js
@@ -3,7 +3,9 @@
 const express = require('express')
 const router = express.Router()
 const tripayCallback = require('../callback/TripayCallback')
+const iakCallback = require('../callback/IakCallback')
 
 router.post('/tripay', tripayCallback.handle)
+router.post('/iak', iakCallback.handle)
 
 module.exports = router
